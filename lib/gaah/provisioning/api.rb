d02556a7523d626c04a6d0cb33eb500a687ca74e
hc5duke <hc5duke@gmail.com>
2013-07-15 00:36:56 -0700

    fetch next

--- lib/gaah/provisioning/api.rb
+++ lib/gaah/provisioning/api.rb
@@ -5,10 +5,24 @@ module Gaah
     class Api
       class << self
         def users
-          url   = "https://apps-apis.google.com/a/feeds/#{Gaah.domain}/user/2.0"
-          xml   = ApiClient.instance.get(url)
-          users = Nokogiri::XML(xml)/:entry
-          User.batch_create(users)
+          fetch_users("https://apps-apis.google.com/a/feeds/#{Gaah.domain}/user/2.0")
+        end
+
+        private
+
+        def fetch_users(url)
+          xml    = ApiClient.instance.get(url)
+          parsed = Nokogiri::XML(xml)
+
+          current_users = User.batch_create(parsed/:entry)
+          next_link     = (parsed/'link[rel=next]').first
+
+          if next_link.nil?
+            current_users
+          else
+            url = next_link.attr('href')
+            current_users + fetch_users(url)
+          end
         end
       end
     end
