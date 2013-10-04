68ef650296c8056084468538043c59d01621d5b1
Hwan-Joon Choi <hc5duke@gmail.com>
2013-10-04 11:49:21 -0700

    more useful error messages

--- lib/gaah/exceptions.rb
+++ lib/gaah/exceptions.rb
@@ -1,6 +1,24 @@
 module Gaah
-  class HTTPForbidden < Exception;end
-  class HTTPUnauthorized < Exception;end
-  class HTTPBadRequest < Exception;end
-  class UnknownHTTPException < Exception;end
+  class HTTPForbidden < StandardError
+    def initialize(msg = nil)
+      info = "You do not have the necessary permissions to perform this action on this domain."
+      super("#{info}#{msg.nil? ? '.' : ': '}#{msg}")
+    end
+  end
+
+  class HTTPUnauthorized < StandardError
+    def initialize(msg = nil)
+      info = "This domain does not support Google Apps."
+      super("#{info}#{msg.nil? ? '.' : ': '}#{msg}")
+    end
+  end
+
+  class HTTPBadRequest < StandardError
+    def initialize(msg = nil)
+      info = "Unsupported action"
+      super("#{info}#{msg.nil? ? '.' : ': '}#{msg}")
+    end
+  end
+
+  class UnknownHTTPException < StandardError;end
 end
