e051316283d0c61f19d62640dd9dd7f0825c8ff3
Hwan-Joon Choi <hc5duke@gmail.com>
2013-10-22 21:30:51 -0700

    Update exception message texts

--- lib/gaah/exceptions.rb
+++ lib/gaah/exceptions.rb
@@ -1,14 +1,14 @@
 module Gaah
   class HTTPForbidden < StandardError
     def initialize(msg = nil)
-      info = "You do not have the necessary permissions to perform this action on this domain."
+      info = "You do not have the necessary permissions to perform this action on this domain"
       super("#{info}#{msg.nil? ? '.' : ': '}#{msg}")
     end
   end
 
   class HTTPUnauthorized < StandardError
     def initialize(msg = nil)
-      info = "This domain does not support Google Apps."
+      info = "This domain or user does not support Google Apps"
       super("#{info}#{msg.nil? ? '.' : ': '}#{msg}")
     end
   end
