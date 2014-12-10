bf7961101a80f2c80e6fd2348545f206107e7911
Luca Bonmassar <luca.bonmassar@gmail.com>
2014-12-09 20:56:51 -0800

    replace provisioning api

--- lib/gaah/base.rb
+++ lib/gaah/base.rb
@@ -10,15 +10,16 @@ require 'queryparams'
 require 'gaah/api_client'
 require 'gaah/api_model'
 require 'gaah/calendar/api'
-require 'gaah/provisioning/api'
+require 'gaah/directory/api'
 require 'gaah/resource/api'
 require 'gaah/exceptions'
+require 'gaah/gaah_oauth'
 
 module Gaah
   class << self
     attr_accessor :domain
-    def setup_oauth(key, secret)
-      ApiClient.setup_oauth(key, secret)
+    def setup_oauth2(key, secret, redirect_uri = nil)
+      Gaah::OAuth.setup_oauth2(key, secret, redirect_uri)
     end
   end
 end
